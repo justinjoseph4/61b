c04fd6fb77a64e97470cf46e8b97b2394d110330
Biz  Whitney <bizwhitney@Bizs-MacBook-Pro.local>
2018-03-07 15:37:12 -0800

    player

--- proj2/byog/TileEngine/Tileset.java
+++ proj2/byog/TileEngine/Tileset.java
@@ -1,6 +1,6 @@
 package byog.TileEngine;
 
-import org.junit.Test;
+
 
 import java.awt.Color;
 
@@ -19,7 +19,7 @@ import java.awt.Color;
  */
 
 public class Tileset {
-    public static TETile PLAYER = new TETile('@', Color.white, Color.black, "player");
+    public static final TETile PLAYER = new TETile('@', Color.white, Color.black, "player");
     public static final TETile WALL = new TETile('#', new Color(216, 128, 128), Color.darkGray,
             "wall");
     public static final TETile FLOOR = new TETile('·', new Color(128, 192, 128), Color.black,
